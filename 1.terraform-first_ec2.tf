8b8dbd62e5e7e4dab656d5a40405232aef5a5606
jaspreet singh <MY_NAME@example.com>
2026-08-01 11:24:03 +1000

    Commented ec2

--- 1.terraform-first_ec2.tf
+++ 1.terraform-first_ec2.tf
@@ -11,11 +11,11 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "ec2_instance" {
-  ami           = "ami-095bd4a11ce8746c0" # Singapore region
-  instance_type = "t2.micro"
-  tags = {
-    Name        = "MyFirstEC2Instance"
-    Environment = "Development"
-  }
-}
+# resource "aws_instance" "ec2_instance" {
+#   ami           = "ami-095bd4a11ce8746c0" # Singapore region
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name        = "MyFirstEC2Instance"
+#     Environment = "Development"
+#   }
+# }
